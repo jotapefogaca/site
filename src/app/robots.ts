0c3abbc264e9a76cd0e626200c696196051a905c
jotapefogaca <joaopedrofsantos14@gmail.com>
2025-06-26 13:29:02 -0300

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/',
+          '/dashboard/',
+          '/_next/',
+          '/private/',
+        ],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/',
+          '/dashboard/',
+          '/private/',
+        ],
+      },
+    ],
+    sitemap: 'https://www.foxylady.com.br/sitemap.xml',
+  }
+}
